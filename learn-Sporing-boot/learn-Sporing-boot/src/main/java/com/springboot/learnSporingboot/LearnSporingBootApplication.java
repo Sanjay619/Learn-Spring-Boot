8d3e6746a1972e4924cdd20ad6774340074d1863
Sanjay <sanjaigoswami619@gmail.com>
2024-05-12 17:00:45 +0500

    leanSpring project"
    This reverts commit e969bf8b7e100842fc80d43b91ae79e06d306e35.

---
+++ learn-Sporing-boot/learn-Sporing-boot/src/main/java/com/springboot/learnSporingboot/LearnSporingBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.learnSporingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSporingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSporingBootApplication.class, args);
+	}
+
+}
---
+++ learn-Sporing-boot/learn-Sporing-boot/src/test/java/com/springboot/learnSporingboot/LearnSporingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.learnSporingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSporingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
